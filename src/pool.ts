4750569e68771b832ad2791718aa146ae81aaf5b
jingyi2811 <jingyi2811@gmail.com>
2023-12-17 18:07:20 +0800

    Initial commit

--- src/pool.ts
+++ src/pool.ts
@@ -1,4 +1,5 @@
 import {BalancerSDK, BalancerSdkConfig, Network, PoolWithMethods} from '@balancer-labs/sdk';
+import Web3 from 'web3';
 
 const config: BalancerSdkConfig = {
     network: Network.MAINNET,
@@ -7,8 +8,15 @@ const config: BalancerSdkConfig = {
 const balancer = new BalancerSDK(config);
 
 const pools: PoolWithMethods[] = await balancer.pools.all();
-const stablePoolIds = pools
-    .filter(pool => pool.poolType === 'Stable')
-    .map(stablePool => stablePool.id);
 
-console.log(stablePoolIds)
+// const stablePoolIds = pools
+//     .filter(pool => pool.poolType === 'Stable')
+//     .map(stablePool => stablePool.id);
+
+const result = pools
+    .filter(pool => pool.poolType === 'Stable')
+   .map(stablePool => stablePool.address);
+
+for (let i = 0; i < result.length; i++) {
+    console.log(Web3.utils.toChecksumAddress(result[i]));
+}
